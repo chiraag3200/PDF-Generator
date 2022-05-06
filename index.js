a7a854c948c8bb1603f8247f627c160b4508fbe5
chiraag3200 <chiraagmittal05@gmail.com>
2022-05-07 00:10:15 +0530

    generate pdf functionality

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs-extra');
+const data = require('./data.json')
+const Handlebars = require('handlebars')
+const path = require('path')
+const moment = require('moment')
+
+const compile = async function(templateName, data){
+	const filepath = path.join(process.cwd(), 'templates', `${templateName}.hbs`)
+	const html = await fs.readFile(filepath, 'utf-8')
+	return Handlebars.compile(html)(data)
+}
+
+var hbs = Handlebars.create({
+  helpers: {
+    dateFormat: function (value, format) { return moment(value).format(format) }  
+  }
+});
+
 
 (async function() {
 	try{
 		const browser = await puppeteer.launch()
 		const page = await browser.newPage()
 
-		await page.setContent('<h1>Hello</h1>')
+		const content = await compile('generate_receipt_template', data)
+
+
+		await page.setContent(content)
 		await page.emulateMediaType('screen')
 		await page.pdf({
 			path: 'mypdf.pdf',
@@ -14,7 +34,6 @@ const fs = require('fs-extra');
 			printBackground: true
 		})
 
-		console.log('done')
 		await browser.close()
 		process.exit()
 	}
